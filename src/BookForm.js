f2c1b2bf082df4d4f1ae73653712c89e3141b509
Srivathsa KM <srivathsakm@gmail.com>
2021-11-01 14:51:30 +0530

    added colour radio button

--- src/BookForm.js
+++ src/BookForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const BookForm = (props) => {
   const { bookFormSubmit, editId, editBookName, editAuthorName, editColour, handleBooks } = props;
@@ -7,6 +7,8 @@ const BookForm = (props) => {
   const [bookName, setBookName] = useState(editBookName ? editBookName : '');
   const [authorName, setAuthorName] = useState(editAuthorName ? editAuthorName : '');
   const [colour, setColour] = useState(editColour ? editColour : '');
+  const [showRed, setShowRed] = useState(false);
+  const [showOthers, setShowOthers] = useState(false);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -24,27 +26,78 @@ const BookForm = (props) => {
     setBookName('');
     setAuthorName('');
     setColour('');
+    setShowRed(false);
+    setShowOthers(false);
   };
+
+  useEffect(() => {
+    if (editColour == 'red') {
+      setShowOthers(true);
+      setShowRed(true);
+    } else {
+      setShowOthers(true);
+      setShowRed(false);
+    }
+  }, []);
 
   return (
     <>
       <br />
       <form onSubmit={handleFormSubmit}>
-        <label>Book Name: </label>
-        <input type='text' value={bookName} onChange={(e) => setBookName(e.target.value)} />
-        <br /> <br />
-        <label>Author Name: </label>
-        <input type='text' value={authorName} onChange={(e) => setAuthorName(e.target.value)} />
+        {showOthers ? (
+          showRed ? (
+            <div>
+              <label>Book Name: </label>
+              <input type='text' value={bookName} onChange={(e) => setBookName(e.target.value)} />
+              <br /> <br />
+            </div>
+          ) : (
+            <div>
+              <label>Author Name: </label>
+              <input type='text' value={authorName} onChange={(e) => setAuthorName(e.target.value)} />
+            </div>
+          )
+        ) : null}
         <br /> <br />
         <p>Colour :</p>
         <div>
-          <input type='radio' value='red' checked={colour === 'red'} onChange={(e) => setColour(e.target.value)} /> Red
+          <input
+            type='radio'
+            value='red'
+            checked={colour === 'red'}
+            onChange={(e) => {
+              setColour(e.target.value);
+              setShowOthers(true);
+              setShowRed(true);
+            }}
+          />
+          Red
         </div>
         <div>
-          <input type='radio' value='green' checked={colour === 'green'} onChange={(e) => setColour(e.target.value)} /> Green
+          <input
+            type='radio'
+            value='green'
+            checked={colour === 'green'}
+            onChange={(e) => {
+              setColour(e.target.value);
+              setShowOthers(true);
+              setShowRed(false);
+            }}
+          />
+          Green
         </div>
         <div>
-          <input type='radio' value='blue' checked={colour === 'blue'} onChange={(e) => setColour(e.target.value)} /> Blue
+          <input
+            type='radio'
+            value='blue'
+            checked={colour === 'blue'}
+            onChange={(e) => {
+              setColour(e.target.value);
+              setShowOthers(true);
+              setShowRed(false);
+            }}
+          />{' '}
+          Blue
         </div>
         <br /> <br />
         <button type='submit'>save</button>
